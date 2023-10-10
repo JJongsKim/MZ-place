96fdae31b17288bc061c0cc6efd043c46cb25fdf
JJongsKim <totnwjd530@gmail.com>
2023-10-10 15:30:07 +0900

    :lipstick: Style: 로그인페이지 소셜로그인 박스 스타일링

--- src/pages/Auth/signIn.tsx
+++ src/pages/Auth/signIn.tsx
@@ -4,11 +4,17 @@ import {
   FormListWrap,
   SignForm,
   SignFormWrap,
+  SocialIcon,
+  SocialText,
+  SocialWrap,
   SubmitText,
   SubmitWrap,
 } from './style';
 import InputBase from '@components/common/InputBase';
 import ButtonBase from '@components/common/ButtonBase';
+import kakao from '@assets/icon-kakao.svg';
+import naver from '@assets/icon-naver.svg';
+
 import { Link } from 'react-router-dom';
 
 const SignIn = () => {
@@ -25,6 +31,11 @@ const SignIn = () => {
               <InputBase placeholder="비밀번호를 입력해주세요!" />
             </li>
           </FormListWrap>
+          <SocialWrap>
+            <SocialText>SNS로 간편 로그인하기</SocialText>
+            <SocialIcon src={kakao} alt="카카오" />
+            <SocialIcon src={naver} alt="네이버" />
+          </SocialWrap>
           <SubmitWrap>
             <Link to="/sign-up">
               <SubmitText>아직 회원이 아니신가요?</SubmitText>
--- src/pages/Auth/style.ts
+++ src/pages/Auth/style.ts
@@ -24,6 +24,9 @@ const SignFormWrap = styled.div`
 
 const SignForm = styled.form`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 400px;
   margin-bottom: 50px;
 
@@ -49,11 +52,56 @@ const SubmitWrap = styled.div`
 const SubmitText = styled.p`
   margin: 10px 0;
   ${({ theme }) => theme.font.SB_12};
-  color: ${({ theme }) => theme.colors.text_gray};
+  color: ${({ theme }) => theme.colors.gray};
 
   @media screen and (min-width: 1200px) {
+    margin: 15px 0;
     ${({ theme }) => theme.font.SB_16};
   }
 `;
 
-export { DeskTopViewWrap, SignFormWrap, SignForm, FormListWrap, SubmitWrap, SubmitText };
+// 로그인 페이지 - 소셜로그인
+const SocialWrap = styled.div`
+  width: 150px;
+  height: 80px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  place-items: center;
+  margin-top: 85px;
+
+  :nth-child(1) {
+    grid-column: 1/3;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 200px;
+    height: 100px;
+    margin-top: 75px;
+  }
+`;
+
+const SocialIcon = styled.img`
+  width: 55px;
+  height: 55px;
+  cursor: pointer;
+
+  @media screen and (min-width: 1200px) {
+    width: 70px;
+    height: 70px;
+  }
+`;
+
+const SocialText = styled(SubmitText)``;
+
+export {
+  DeskTopViewWrap,
+  SignFormWrap,
+  SignForm,
+  FormListWrap,
+  SubmitWrap,
+  SubmitText,
+  SocialWrap,
+  SocialIcon,
+  SocialText,
+};
