99cd27b10f7f52db86d672788e8f63cb6f942299
JJongsKim <totnwjd530@gmail.com>
2023-10-12 15:23:02 +0900

    :lipstick: Style: 탐색 페이지 스타일링

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,7 +1,43 @@
 import SearchBar from '@components/common/SearchBar';
 
+import {
+  MenuDivideLine,
+  MenuIcon,
+  MenuListWrap,
+  MenuName,
+  MenuWrap,
+  SearchPageWrap,
+} from './style';
+import { CUSTOM_MENU, MENU } from '@application/constant';
+import { Link } from 'react-router-dom';
+
 const SearchPage = () => {
-  return <SearchBar name="실외활동 탐색" searchIcon={true} />;
+  return (
+    <SearchPageWrap>
+      <SearchBar name="실외활동 탐색" searchIcon={true} />
+      <MenuListWrap>
+        {MENU.map(menu => (
+          <Link to={menu.path} key={menu.name}>
+            <MenuWrap>
+              <MenuIcon src={menu.svg} />
+              <MenuName>{menu.name}</MenuName>
+            </MenuWrap>
+          </Link>
+        ))}
+      </MenuListWrap>
+      <MenuDivideLine />
+      <MenuListWrap>
+        {CUSTOM_MENU.map(custom => (
+          <Link to={custom.path} key={custom.name}>
+            <MenuWrap>
+              <MenuIcon src={custom.svg} />
+              <MenuName>{custom.name}</MenuName>
+            </MenuWrap>
+          </Link>
+        ))}
+      </MenuListWrap>
+    </SearchPageWrap>
+  );
 };
 
 export default SearchPage;
