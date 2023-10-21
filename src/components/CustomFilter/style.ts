c73d1727d53ca406cfb260abccbfa04605194706
JJongsKim <totnwjd530@gmail.com>
2023-10-21 20:53:01 +0900

    :lipstick: Style: 맞춤 필터 스타일링

---
+++ src/components/CustomFilter/style.ts
@@ -0,0 +1,35 @@
+import { styled } from 'styled-components';
+
+const CustomFilterWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 25px;
+`;
+
+const FilterTypeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 25px;
+`;
+
+const FilterTitle = styled.p`
+  margin-bottom: 10px;
+  ${({ theme }) => theme.font.B_14};
+`;
+
+const FilterList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+
+  li {
+    padding: 10px 8px;
+  }
+`;
+
+const FilterButtonWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+
+export { CustomFilterWrap, FilterTypeContainer, FilterTitle, FilterList, FilterButtonWrap };
