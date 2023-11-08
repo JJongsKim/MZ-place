80918c511f198320419fd663bd0db444fc324741
JJongsKim <totnwjd530@gmail.com>
2023-11-08 20:09:52 +0900

    :construction: Push 테스트

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -9,7 +9,7 @@ const MainPage = () => {
       <AutoSlide />
       <RecommendTextWrap>
         <RecommendText>서울산책 회원들이 추천하는</RecommendText>
-        <RecommendTop20>TOP20</RecommendTop20>
+        <RecommendTop20>TOP20 테스트</RecommendTop20>
       </RecommendTextWrap>
       <ThumbnailList />
     </MainPageWrap>
