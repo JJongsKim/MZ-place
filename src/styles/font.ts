672772854a2fdbfaf3c12f1c2fa7081325f88b32
JJongsKim <totnwjd530@gmail.com>
2023-11-13 01:46:10 +0900

    :bug: Fix: 불러오는 웹폰트로 변경/IOS-safari 폰트 적용 확인
    - 저장하고 직접 불러오던 Import 폰트 삭제
    - Noto Sans KR 웹폰트 link 태그로 불러오기
    - Safari 폰트 적용 확인

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 type Font = {
   size: number;
   weight: 'R' | 'M' | 'SB' | 'B';
-  family: 'pretendard';
+  family: 'Noto Sans KR';
 };
 
 const getFontWeight = (weight: Font['weight']) => {
@@ -26,29 +26,29 @@ const FONT = ({ size, weight, family }: Font) => css`
 `;
 
 export const font = {
-  M_12: FONT({ size: 12, family: 'pretendard', weight: 'M' }),
-  M_14: FONT({ size: 14, family: 'pretendard', weight: 'M' }),
-  M_16: FONT({ size: 16, family: 'pretendard', weight: 'M' }),
-  M_18: FONT({ size: 18, family: 'pretendard', weight: 'M' }),
-  M_20: FONT({ size: 20, family: 'pretendard', weight: 'M' }),
-  M_24: FONT({ size: 24, family: 'pretendard', weight: 'M' }),
+  M_12: FONT({ size: 12, family: 'Noto Sans KR', weight: 'M' }),
+  M_14: FONT({ size: 14, family: 'Noto Sans KR', weight: 'M' }),
+  M_16: FONT({ size: 16, family: 'Noto Sans KR', weight: 'M' }),
+  M_18: FONT({ size: 18, family: 'Noto Sans KR', weight: 'M' }),
+  M_20: FONT({ size: 20, family: 'Noto Sans KR', weight: 'M' }),
+  M_24: FONT({ size: 24, family: 'Noto Sans KR', weight: 'M' }),
 
-  SB_12: FONT({ size: 12, family: 'pretendard', weight: 'SB' }),
-  SB_14: FONT({ size: 14, family: 'pretendard', weight: 'SB' }),
-  SB_16: FONT({ size: 16, family: 'pretendard', weight: 'SB' }),
-  SB_18: FONT({ size: 18, family: 'pretendard', weight: 'SB' }),
-  SB_20: FONT({ size: 20, family: 'pretendard', weight: 'SB' }),
-  SB_22: FONT({ size: 22, family: 'pretendard', weight: 'SB' }),
-  SB_23: FONT({ size: 23, family: 'pretendard', weight: 'SB' }),
+  SB_12: FONT({ size: 12, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_14: FONT({ size: 14, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_16: FONT({ size: 16, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_18: FONT({ size: 18, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_20: FONT({ size: 20, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_22: FONT({ size: 22, family: 'Noto Sans KR', weight: 'SB' }),
+  SB_23: FONT({ size: 23, family: 'Noto Sans KR', weight: 'SB' }),
 
-  B_13: FONT({ size: 13, family: 'pretendard', weight: 'B' }),
-  B_14: FONT({ size: 14, family: 'pretendard', weight: 'B' }),
-  B_15: FONT({ size: 15, family: 'pretendard', weight: 'B' }),
-  B_16: FONT({ size: 16, family: 'pretendard', weight: 'B' }),
-  B_18: FONT({ size: 18, family: 'pretendard', weight: 'B' }),
-  B_20: FONT({ size: 20, family: 'pretendard', weight: 'B' }),
-  B_22: FONT({ size: 22, family: 'pretendard', weight: 'B' }),
-  B_24: FONT({ size: 24, family: 'pretendard', weight: 'B' }),
-  B_26: FONT({ size: 26, family: 'pretendard', weight: 'B' }),
-  B_28: FONT({ size: 28, family: 'pretendard', weight: 'B' }),
+  B_13: FONT({ size: 13, family: 'Noto Sans KR', weight: 'B' }),
+  B_14: FONT({ size: 14, family: 'Noto Sans KR', weight: 'B' }),
+  B_15: FONT({ size: 15, family: 'Noto Sans KR', weight: 'B' }),
+  B_16: FONT({ size: 16, family: 'Noto Sans KR', weight: 'B' }),
+  B_18: FONT({ size: 18, family: 'Noto Sans KR', weight: 'B' }),
+  B_20: FONT({ size: 20, family: 'Noto Sans KR', weight: 'B' }),
+  B_22: FONT({ size: 22, family: 'Noto Sans KR', weight: 'B' }),
+  B_24: FONT({ size: 24, family: 'Noto Sans KR', weight: 'B' }),
+  B_26: FONT({ size: 26, family: 'Noto Sans KR', weight: 'B' }),
+  B_28: FONT({ size: 28, family: 'Noto Sans KR', weight: 'B' }),
 };
