5ecf79557096e9a3ba024ac833f410a10628e34b
JJongsKim <totnwjd530@gmail.com>
2023-11-15 20:56:16 +0900

    :sparkles: Feat: Reqct-Query places 관련 맞춤 필터 hook 정의

---
+++ src/hooks/api/places/index.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { api } from '@infra/api';
+
+export const useGetPlacesOfFilter = (queryParams: Record<string, string | number>) => {
+  const { data } = useQuery({
+    queryKey: ['getPlacesOfFilter'],
+    queryFn: () => api.places.getPlacesOfFilter(queryParams),
+  });
+  console.log(data);
+  return data;
+};
