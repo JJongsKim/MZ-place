a3774ce3053ad0a7505425aeeeda6a687c426196
JJongsKim <totnwjd530@gmail.com>
2023-11-18 23:59:59 +0900

    :sparkles: Feat: zoom level, 지도 영역(북동쪽-남서쪽) 위도경도 구하기
    - 지도 드래그 이벤트가 끝난 후 불러오기

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { REGION_ARRAY } from '@application/constant';
 import Loading from '@components/common/Loading';
 import useGeoLocation from '@hooks/useGeoLocation';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { styled } from 'styled-components';
 
 interface MapProps {
@@ -14,6 +15,8 @@ const geoLocationOptions = {
 };
 
 const Map = ({ currentAddress }: MapProps) => {
+  const mapRef = useRef(null);
+
   const { location } = useGeoLocation(geoLocationOptions);
   const currentLocation = {
     latitude: location.latitude,
@@ -50,6 +53,25 @@ const Map = ({ currentAddress }: MapProps) => {
         }
 
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
+        mapRef.current = map; // 지도 객체 저장해놓기
+
+        // 지도 레벨 6이하일때만 API 호출
+        window.kakao.maps.event.addListener(map, 'dragend', function () {
+          const bounds = map.getBounds();
+
+          window.kakao.maps.event.addListener(map, 'zoom_changed', function () {
+            const level = map.getLevel();
+
+            if (level <= 6) {
+              const zoomBounds = map.getBounds();
+              console.log('줌+ 북동쪽 위도경도', zoomBounds.getNorthEast());
+              console.log('줌+ 남서쪽 위도경도', zoomBounds.getSouthWest());
+            }
+          });
+
+          console.log('마지막 북동쪽 위도경도', bounds.getNorthEast());
+          console.log('마지막 남서쪽 위도경도', bounds.getSouthWest());
+        });
 
         return map;
       });
