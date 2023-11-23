fc58e4ee5340d714b81ae3cf9080d01225f24602
JJongsKim <totnwjd530@gmail.com>
2023-11-23 23:32:20 +0900

    :sparkles: Feat: 메인페이지 장소 Top20 API 연결
    - TODO: top20에서의 데이터 속성명이 달라서 ThumbnailList/ThumbnailBox 수정 필요

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,14 +2,19 @@ import ThumbnailList from '@components/common/ThumbnailList';
 import AutoSlide from '@components/MainPage/AutoSlide';
 
 import { MainPageWrap, RecommendText, RecommendTextWrap, RecommendTop20 } from './style';
+import { useGetPlacesOfTop20 } from '@hooks/api/places';
 
 const MainPage = () => {
+  const top20Places = useGetPlacesOfTop20();
+  console.log('TOP20:::', top20Places.data);
+
   return (
     <MainPageWrap>
       <AutoSlide />
       <RecommendTextWrap>
         <RecommendText>서울산책 회원들이 추천하는</RecommendText>
         <RecommendTop20>TOP20</RecommendTop20>
+        {/* {top20Places !== undefined && <ThumbnailList places={top20Places} />} */}
       </RecommendTextWrap>
       <ThumbnailList />
     </MainPageWrap>
