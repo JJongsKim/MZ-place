41151c6c23ed445da610782daf1e0bffc0bf1b57
JJongsKim <totnwjd530@gmail.com>
2023-11-26 20:27:20 +0900

    :memo: Docs: react-query devtools 주석 처리

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
@@ -16,7 +16,6 @@ root.render(
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
