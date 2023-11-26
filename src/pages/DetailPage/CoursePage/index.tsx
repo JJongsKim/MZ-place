0959f38a92155c103d8727d42c25079d0079dc14
JJongsKim <totnwjd530@gmail.com>
2023-11-26 20:31:55 +0900

    :rocket: Setting: 사용자용 1차 배포

--- src/pages/DetailPage/CoursePage/index.tsx
+++ src/pages/DetailPage/CoursePage/index.tsx
@@ -1,6 +1,7 @@
 import SearchBar from '@components/common/SearchBar';
 import { DetailPageWrap } from '../style';
 import { useLocation } from 'react-router-dom';
+import WarningMention from '@components/common/warning';
 
 const CoursePage = () => {
   const location = useLocation();
@@ -8,6 +9,7 @@ const CoursePage = () => {
   return (
     <DetailPageWrap>
       <SearchBar name={`${location.state.name}`} backIcon={true} searchIcon={true} />
+      <WarningMention text="미래유산코스 페이지는 아직 개발중이에요 :D" />
     </DetailPageWrap>
   );
 };
--- src/pages/DetailPage/LikeRecommendPage/index.tsx
+++ src/pages/DetailPage/LikeRecommendPage/index.tsx
@@ -2,6 +2,7 @@ import SearchBar from '@components/common/SearchBar';
 import { DetailPageWrap } from '../style';
 import { useLocation } from 'react-router-dom';
 import withAuth from '@components/HOC/withAuth';
+import WarningMention from '@components/common/warning';
 
 const LikeRecommendPage = () => {
   const location = useLocation();
@@ -9,6 +10,7 @@ const LikeRecommendPage = () => {
   return (
     <DetailPageWrap>
       <SearchBar name={`${location.state.name}`} backIcon={true} searchIcon={true} />
+      <WarningMention text="사용자 추천 찜 페이지는 아직 개발중이에요 :D" />
     </DetailPageWrap>
   );
 };
